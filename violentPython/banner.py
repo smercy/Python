a518375e7cb9e7a023135a84e3b84cdf6cec96e2
smercy <stevemercy4@gmail.com>
2018-01-11 22:13:53 -0600

    more files and changes

--- violentPython/banner.py
+++ violentPython/banner.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import sys
 
 
 def retBanner(ip, port):
@@ -12,24 +14,38 @@ def retBanner(ip, port):
         print(str(e))
 
 
-def checkVulns(banner):
-    if "(vsFTPd 2.3.4)" in banner:
-        print("[+] vsFTPd FTP server vulnerable")
-    elif '3COM 3CDaemon FTP Server Version 2.0' in banner:
-        print("[+] 3COM 3CDaemon FTP Server vulnerable")
-    else:
-        print("[-] FTP Server is not vulnerable")
+def checkVulns(banner, file):
+    f = open(file, 'r')
+    for line in f.readlines():
+        if line.strip('\n') in banner:
+            print("[+] Server is vulnerable: " + banner.strip('\n'))
+        else:
+            print("[-] FTP Server is not vulnerable")
+        return
 
 
 def main():
-    portList = [21, 22, 25, 80, 110, 443]
-    for x in range(123, 128):
+    if len(sys.argv) == 2:
+        fileName = sys.argv[1]
+        if not os.path.isfile(fileName):
+            print("[-] " + fileName + "Does not exist")
+            exit(0)
+            if not os.access(fileName, os.R_OK):
+                print("[-] " + fileName + "Access denied")
+                exit(0)
+        else:
+            print("[-] Usage: " + str(sys.argv[0]) + "<vulnerable fileName>")
+
+        print("[+] Reading vulnerabilities from " + fileName)
+
+    portList = [21]
+    for x in range(124, 125):
         ip = '172.16.133.' + str(x)
         for port in portList:
             banner = retBanner(ip, port)
             if banner:
                 print("[+]" + ip + ":" + banner.strip('\n'))
-                checkVulns(banner)
+                checkVulns(banner, fileName)
 
 
 if __name__ == '__main__':
